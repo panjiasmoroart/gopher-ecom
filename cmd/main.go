65a4cc1ecc705733a3991f87f72453ae985bc711
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-05-09 09:01:12 +0700

    API - Database Connection & Envs

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/panjiasmoroart/gopher-ecom/cmd/api"
+	"github.com/panjiasmoroart/gopher-ecom/configs"
+	"github.com/panjiasmoroart/gopher-ecom/db"
 )
 
 func main() {
+	cfg := mysql.Config{
+		User:                 configs.Envs.DBUser,
+		Passwd:               configs.Envs.DBPassword,
+		Addr:                 configs.Envs.DBAddress,
+		DBName:               configs.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	}
+
+	db, err := db.NewMySQLStorage(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
 	server := api.NewAPIServer(":9090", nil)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB: Successfully connected!")
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/panjiasmoroart/gopher-ecom
 
 go 1.23.1
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require github.com/go-sql-driver/mysql v1.9.2
+
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/joho/godotenv v1.5.1
+)
